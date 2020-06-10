9d173bb5e7e99527285e762b2a9f5dcb49cfe913
Abdoulaye-Thespy <njigouhrazak@iut-dhaka.edu>
2020-06-10 19:49:07 +0100

    corrected linters errors as required by the TSE

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
 class Comment < ApplicationRecord
-  validates :body, length: { minimum: 1 }
-  validates :user_id, presence: true
-  validates :post_id, presence: true
+  validates :body, presence: true
   belongs_to :user
   belongs_to :post
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,6 @@
 class Post < ApplicationRecord
   validates :title, uniqueness: true, presence: true, length: { minimum: 10 }
   validates :body, presence: true
-  validates :user_id, presence: true
   belongs_to :user
   has_many :comments
 end
