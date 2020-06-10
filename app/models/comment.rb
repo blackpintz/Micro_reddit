66e8e877eaa2a32556d97b3eb89c5a2e184f1710
Abdoulaye-Thespy <njigouhrazak@iut-dhaka.edu>
2020-06-10 11:09:13 +0100

    first part of association

---
+++ app/models/comment.rb
@@ -0,0 +1,7 @@
+class Comment < ApplicationRecord
+	validates :body, length: { minimum: 1 }
+	validates :user_id, presence: true
+	validates :post_id, presence: true
+	belongs_to :user
+	belongs_to :post
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
 	validates :name, uniqueness: true, length: { minimum: 4 }, presence: true
 	has_many :posts
+	has_many :comments, through: :posts
 end
